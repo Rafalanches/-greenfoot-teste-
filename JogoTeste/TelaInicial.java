a10c56d68688532a83c4b15956a106e3df5a2752
Rafalanches <133991142+Rafalanches@users.noreply.github.com>
2023-10-24 10:43:42 -0300

    Add files via upload

---
+++ JogoTeste/TelaInicial.java
@@ -0,0 +1,76 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TelaInicial here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TelaInicial extends World
+{
+    private int atraso = 0;
+    private GreenfootSound liMusic = new GreenfootSound("glamour.mp3");
+    
+    public void started()
+    {
+    liMusic.playLoop();
+    }
+    
+     public void stopped()
+    {
+    liMusic.pause();
+    }
+    
+    /**
+     * Constructor for objects of class TelaInicial.
+     * 
+     */
+    public TelaInicial()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 500, 1); 
+        prepare();
+    }
+
+    public void act(){
+
+        if (Greenfoot.isKeyDown("enter") && (atraso == 0)){
+            setBackground(new GreenfootImage("historinha2.png"));
+            atraso++;
+        }
+        else{
+            if (Greenfoot.isKeyDown("enter")){
+                atraso=0;
+            }
+        }
+
+          if (Greenfoot.isKeyDown("enter") && (atraso == 0)){
+            setBackground(new GreenfootImage("historinha1.png"));
+            atraso++;
+        }
+        else{
+            if (Greenfoot.isKeyDown("enter")){
+                atraso=0;
+            }
+        }
+        
+         if (Greenfoot.isKeyDown("enter") && (atraso == 0)){
+            Mundão world = new Mundão();
+            Greenfoot.setWorld(world);
+            atraso++;
+        }
+        else{
+            if (Greenfoot.isKeyDown("enter")){
+                atraso=0;
+            }
+        }
+    }
+    
+    /**
+     * Prepara o mundo para o início do programa.
+     * Ou seja: criar os objetos iniciais e adicioná-los ao mundo.
+     */
+    private void prepare()
+    {
+    }
+}
